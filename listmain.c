30cd78c48ff7da0aef93e2abf8dd309300d3a043
Pedro <pedro.souza.psj@gmail.com>
2017-03-13 19:15:35 -0400

    biblioteca da lista encadeada adicionada e funfa

---
+++ listmain.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "list.h"
+
+int main() {
+
+	/*Driver code to test the implementation*/
+	head = NULL; // empty list. set head as NULL. 
+	
+	// Calling an Insert and printing list both in forward as well as reverse direction. 
+	InsertAtTail(2); Print(); ReversePrint();
+	InsertAtTail(4); Print(); ReversePrint();
+	InsertAtHead(6); Print(); ReversePrint();
+	InsertAtTail(8); Print(); ReversePrint();
+	
+}
